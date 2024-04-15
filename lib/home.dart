1c4ab22ce58befb6024435ff75d8e65cf508c5e8
Wellington Luiz da Silva Junior <wellingtonluiz.silva-ext@ab-inbev.com>
2024-04-15 11:03:18 -0300

    Ajustando layout, funcionamento base da tela e modal de criacao

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 import 'package:firebase_database/firebase_database.dart';
 
@@ -27,7 +26,7 @@ class _HomePageState extends State<HomePage> {
 
   String selectedValue = 'Jheni';
   String? textInput;
-  String? dateInput;
+
   @override
   void initState() {
     super.initState();
@@ -44,168 +43,205 @@ class _HomePageState extends State<HomePage> {
           snapshot.snapshot.value as Map<dynamic, dynamic>;
       values.forEach((key, value) {
         setState(() {
-          items.add(ChecklistItem(id: key, pessoa: nome, title: value["atividade"], isSelected: value["feito"] == 1 ? true : false));
+          items.add(ChecklistItem(
+              id: key,
+              pessoa: nome,
+              title: value["atividade"],
+              isSelected: value["feito"] == 1 ? true : false));
         });
       });
     });
-  } 
-  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffefdfd7),
       body: FutureBuilder(
         future: todosFuture,
         builder: (context, snapshot) {
           return SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: 200,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: images.length,
-                  shrinkWrap: true,
-                  physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    return GestureDetector(
-                      onTap: () {
-                        setState(() {
-                          selectedImages = List<bool>.filled(images.length, false);
-                          selectedImages[index] = !selectedImages[index];
-                        });
-                        String arquivo = images[index].split('/').last;
-                        getTodos(arquivo.split('.').first);
-                      },
-                      child: Stack(
-                        alignment: Alignment.center,
-                        children: [
-                          if (selectedImages[index])
-                            CircleAvatar(
-                              radius: 100,
-                              backgroundColor: Colors.blue.withOpacity(0.3),
-                            ),
-                          AspectRatio(
-                            aspectRatio: 1,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: ClipOval(
-                                child: Image.asset(
-                                  images[index],
-                                  fit: BoxFit.cover,
-                                ),
-                              ),
-                            ),
-                          ),
-                          
-                        ],
-                      ),
-                    );
-                  },
-                ),
-              ),
-              Column(
-                children: [
-                  ListView.builder(
-                    itemCount: items.length,
-                    shrinkWrap: true,
-                    physics: const NeverScrollableScrollPhysics(),
-                    itemBuilder: (context, index) {
-                      return Card(
-                        color: items[index].isSelected ? Colors.green : null,
-                        child: ListTile(
-                          title: Text(items[index].title),
-                          onTap: () async {
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: SizedBox(
+                    height: 200,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: images.length,
+                      shrinkWrap: true,
+                      itemBuilder: (context, index) {
+                        return GestureDetector(
+                          onTap: () {
+                            String arquivo = images[index].split('/').last;
+                            getTodos(arquivo.split('.').first);
+
                             setState(() {
-                              items[index].isSelected = !items[index].isSelected;
-                            });
-                            DatabaseReference ref = FirebaseDatabase.instance.ref(items[index].pessoa).child(items[index].id);
-                            await ref.update({
-                              "feito": items[index].isSelected ? 1 : 0,
+                              selectedImages =
+                                  List<bool>.filled(images.length, false);
+                              selectedImages[index] = !selectedImages[index];
+                              selectedValue =
+                                  arquivo.split('.').first.toCapitalized();
                             });
                           },
-                        ),
-                      );
-                    },
+                          child: Stack(
+                            alignment: Alignment.center,
+                            children: [
+                              if (selectedImages[index])
+                                const CircleAvatar(
+                                  radius: 100,
+                                  backgroundColor: Color(0xffb5b4a3),
+                                ),
+                              AspectRatio(
+                                aspectRatio: 1,
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: ClipOval(
+                                    child: Image.asset(
+                                      images[index],
+                                      fit: BoxFit.cover,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
                   ),
-                ],
-              ),
-            ],
-          ),
-        );
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(30.0),
+                  child: Column(
+                    children: [
+                      ListView.builder(
+                        itemCount: items.length,
+                        shrinkWrap: true,
+                        physics: const NeverScrollableScrollPhysics(),
+                        itemBuilder: (context, index) {
+                          return Card(
+                            color: items[index].isSelected
+                                ? const Color(0xffb5b4a3)
+                                : Colors.white,
+                            child: ListTile(
+                              title: Text(items[index].title,
+                                  style: TextStyle(
+                                      color: items[index].isSelected
+                                          ? Colors.white
+                                          : Colors.black)),
+                              onTap: () async {
+                                setState(() {
+                                  items[index].isSelected =
+                                      !items[index].isSelected;
+                                });
+                                DatabaseReference ref = FirebaseDatabase
+                                    .instance
+                                    .ref(items[index].pessoa)
+                                    .child(items[index].id);
+                                await ref.update({
+                                  "feito": items[index].isSelected ? 1 : 0,
+                                });
+                              },
+                            ),
+                          );
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          );
         },
-
-        ),
+      ),
       floatingActionButton: FloatingActionButton(
+        backgroundColor: const Color(0xffaead98),
         onPressed: () {
           showDialog(
             context: context,
             builder: (BuildContext context) {
-
-              return AlertDialog(
-                title: const Text('Selecione uma pessoa'),
-                content: Column(
+              return Dialog(
+                backgroundColor: Color.fromARGB(255, 243, 229, 220),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
-                    DropdownButton(
-                      hint: const Text('Selecione uma opção'),
-                      value: selectedValue,
-                      items: [
-                        'Jheni',
-                        'Well',
-                        'Alice',
-                        'Helena',
-                        'Julia'
-                      ].map((String value) {
-                        return DropdownMenuItem(
-                          value: value,
-                          child: Text(value.toString()),
-                        );
-                      }).toList(),
-                      onChanged: (newValue) {
-                        setState(() {
-                          selectedValue = newValue!;
-                        });
-                      },
-                    ),
-                    TextField(
-                      onChanged: (value) {
-                        textInput = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: 'Adicione a atividade',
+                    Center(
+                        child: Text(
+                      selectedValue,
+                      style: const TextStyle(
+                        fontSize: 40,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffaead98),
+                      ),
+                    )),
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: TextField(
+                        onChanged: (value) {
+                          textInput = value;
+                        },
+                        decoration: InputDecoration(
+                          focusedBorder: const OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: Color(0xffaead98), width: 5.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                            borderSide: BorderSide(
+                                color: const Color(0xffaead98).withOpacity(0.5),
+                                width: 5.0),
+                          ),
+                          hintText: 'Adicione a atividade',
+                        ),
                       ),
                     ),
-                    TextFormField(
-                      onChanged: (value) {
-                        dateInput = value;
-                      },
-                      decoration: const InputDecoration(
-                        hintText: 'Periodico ou apenas hoje?',
+                    Padding(
+                      padding: const EdgeInsets.all(30.0),
+                      child: SizedBox(
+                        width: double.infinity,
+                        height: 50,
+                        child: ElevatedButton(
+                          style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.resolveWith<Color?>(
+                              (states) => const Color(0xffaead98),
+                            ),
+                              shape: MaterialStateProperty.all<
+                                      RoundedRectangleBorder>(
+                                  RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ))),
+                          onPressed: () {
+                            DatabaseReference ref = FirebaseDatabase.instance
+                                .ref(selectedValue.toLowerCase())
+                                .push();
+                            ref.set({
+                              "atividade": textInput,
+                              "feito": 0,
+                            });
+                            Navigator.of(context).pop();
+                          },
+                          child: const Text('Enviar', style: TextStyle(color: Colors.white, fontSize: 20),),
+                        ),
                       ),
                     ),
                   ],
                 ),
-                actions: <Widget>[
-                  ElevatedButton(
-                    child: const Text('Enviar'),
-                    onPressed: () async {
-                      DatabaseReference ref = FirebaseDatabase.instance.ref(selectedValue!.toLowerCase()).push();
-                      await ref.set({
-                        "atividade": textInput,
-                        "feito": 0,
-                        "periodico" : dateInput,
-                      });
-                      Navigator.of(context).pop();
-                    },
-                  ),
-                ],
               );
             },
           );
         },
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+        ),
       ),
     );
   }
@@ -217,5 +253,18 @@ class ChecklistItem {
   String id;
   String pessoa;
 
-  ChecklistItem({required this.id, required this.title, required this.isSelected, required this.pessoa});
+  ChecklistItem(
+      {required this.id,
+      required this.title,
+      required this.isSelected,
+      required this.pessoa});
+}
+
+extension StringCasingExtension on String {
+  String toCapitalized() =>
+      length > 0 ? '${this[0].toUpperCase()}${substring(1).toLowerCase()}' : '';
+  String toTitleCase() => replaceAll(RegExp(' +'), ' ')
+      .split(' ')
+      .map((str) => str.toCapitalized())
+      .join(' ');
 }
